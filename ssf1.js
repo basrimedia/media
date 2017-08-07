82d7b933257306249e2e2869b4a40371e540973c
basrimedia <basrimedia@gmail.com>
2017-08-07 10:27:16 +0800

    Update and rename ssf.js to ssf1.js

--- ssf1.js
+++ ssf1.js
@@ -23,7 +23,7 @@ var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
 if(x=="1"){xxx="lorem-ipsum-dolor-sit-amet-consectetur.html"}
 if(x=="2"){xxx="fusce-feugiat-condimentum-nulla.html"}
-if(x=="3"){xxx="aliquam-nulla-sapien-sed-facilisis.html"}a_to_ve[i].href="https://johnyaja.blogspot.com/2014/02/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="3"){xxx="aliquam-nulla-sapien-sed-facilisis.html"}a_to_ve[i].href="http://johnyaja.blogspot.com/2014/02/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
